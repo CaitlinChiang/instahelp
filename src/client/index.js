2b86bfd4d66724931394f4959d96bc7d08be2e5e
张志哲 <jeriel@jerieltiu.xyz>
2020-11-28 10:47:56 +0800

    Fixed helper file routing

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Community_Feed from './community_feed'
 import Post_Add from './post_add'
+import Helper_Add from './helper_add'
 
 function Client() {
     return (
@@ -9,6 +10,7 @@ function Client() {
             <Switch>
                 <Route exact path="/" component={Community_Feed} />
                 <Route exact path="/add-help" component={Post_Add} />
+                <Route exact path="/helper-add" component={Helper_Add} />
             </Switch>
         </Router>
     )
