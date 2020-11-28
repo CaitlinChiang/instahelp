ccda6a8d11fd8356026120135ba5018f96db4a55
Lenz Dagohoy <61285670+jacezearo@users.noreply.github.com>
2020-11-28 14:52:36 +0800

    draft need to save

--- src/client/index.js
+++ src/client/index.js
@@ -6,8 +6,8 @@ import Helper_Add from './helper_add'
 
 function Client() {
     return (
-        <Router>
-            <Switch>
+       <Router>
+           <Switch>
                 <Route exact path="/" component={Community_Feed} />
                 <Route exact path="/add-help" component={Post_Add} />
                 <Route exact path="/helper-add" component={Helper_Add} />
@@ -18,14 +18,22 @@ function Client() {
 
 export default Client
 
-<link rel="stylesheet" href="style.css"/>
+//class Client extends Component {
+//    render() {
+//        return (
+//            <div className="home-nav">
+//                <a className="active" href="#home">Home</a>
+//                <a href="#about">About</a>
+//                <a href="#community">Community</a>
+//                <div className="logo">
+//                    <img src="C:\Users\lenzd\OneDrive\Documents\GitHub\instahelp\pics\instahelp.png" class="logo">
+//                    </img>
+//                </div>
+//                <button className="button">Login</button>
+//                </div>
+//        )
+//        
+//    }
+//}
 
-<div class="home-nav">
-    <a class="active" href="#home">Home</a>
-    <a href="#about">About</a>
-    <a href="#community">Community</a>
-    <div class="logo">
-        <img src="C:\Users\lenzd\OneDrive\Documents\GitHub\instahelp\pics\instahelp.png" class="logo">
-    </img></div>
-    <button class="button">Login</button>
-</div>
+//export default Client;
